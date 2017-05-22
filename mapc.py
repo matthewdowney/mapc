20b70790ae30e75799dbded1378cb04b1bea671f
Matthew Downey <matthewdowney20@gmail.com>
2017-05-22 11:24:38 -0500

    added -O flag to specify ONLY certain directories should be considered

--- mapc.py
+++ mapc.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-flags = ["o", "i", "c"]
+flags = ["O", "o", "i", "c"]
 
 def getArg(flag, args):
     if flag not in args: return []
@@ -36,6 +36,7 @@ if len(sys.argv) == 1:
     print("Usage:\n\t%s -c <command>\n" % sys.argv[0])
     print("Options\n\t-o dirs ...\tDirectories for which the command will be executed first, in the order specified.\n" +
           "\t-i dirs ...\tDirectories which will be ignored.\n" +
+          "\t-O dirs ...\tThe ONLY directories for which the command will be executed, in order. Supercedes -o but takes -i into account.\n" +
           "\t-c command\tThe command to execute within each directory.\n")
     print("For example, if the current working directory contains many individual git\n" +
           "repositories and one non-git directory, to pull on each repository: \n\t" +
@@ -44,11 +45,13 @@ if len(sys.argv) == 1:
 
 # Parse the command and directories to prioritize/ignore from the flags
 get = lambda flag: getArg(flag, sys.argv)
+only = get("-O")
 ordered = get("-o")
 ignores = get("-i")
 command = " ".join(get("-c"))
 
 # Map relative directories -> abspaths
+only = map(os.path.abspath, only)
 ordered = map(os.path.abspath, ordered)
 ignores = map(os.path.abspath, ignores)
 
@@ -58,6 +61,11 @@ if ordered != filter(os.path.isdir, ordered):
     print "Ignoring non directories %s" % filter(lambda x: not os.path.isdir(x), ordered)
 ordered = filter(os.path.isdir, ordered)
 
+only = filter(lambda x: x not in ignores, only)
+if only != filter(os.path.isdir, only):
+    print "Ignoring non directories %s" % filter(lambda x: not os.path.isdir(x), only)
+only = filter(os.path.isdir, only)
+
 # The rest of the directories are then those that aren't ordered but are in the working directory
 rest = filter(os.path.isdir, os.listdir(os.getcwd()))
 rest = map(os.path.abspath, rest)
@@ -65,5 +73,8 @@ rest = filter(lambda x: x not in ignores and x not in ordered, rest)
 
 # Execute the command across the ordered directories, and then the rest
 execCmd = lambda x: execute(x, command)
-map(execCmd, ordered)
-map(execCmd, rest)
+if len(only) != 0:
+    map(execCmd, only)
+else:
+    map(execCmd, ordered)
+    map(execCmd, rest)
